b1a25e36fb1b578d806507c9fcaab8091e78fccd
khanhduyapt <khanhduyapt@gmail.com>
2023-02-27 18:00:28 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/repository/OrdersRepository.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/repository/OrdersRepository.java
@@ -20,6 +20,6 @@ public interface OrdersRepository extends JpaRepository<Orders, String> {
     @Query(value = "SELECT * FROM public.orders det where (det.gecko_id like '%HOUR_4') and (det.note <> '') and det.gecko_id = (select REPLACE(mst.gecko_id, 'DAY', 'HOUR_4') from orders mst where det.trend=mst.trend and mst.gecko_id=REPLACE(det.gecko_id, 'HOUR_4', 'DAY')) ORDER BY det.trend, det.gecko_id ", nativeQuery = true)
     public List<Orders> getTrend_H4List();
 
-    @Query(value = "SELECT * FROM public.orders det where (det.gecko_id like '%HOUR')                        and det.gecko_id = (select REPLACE(mst.gecko_id, 'DAY', 'HOUR')   from orders mst where det.trend=mst.trend and mst.gecko_id=REPLACE(det.gecko_id, 'HOUR'  , 'DAY')) ORDER BY det.trend, det.gecko_id ", nativeQuery = true)
+    @Query(value = "SELECT * FROM public.orders det where (det.gecko_id like '%HOUR')   and (det.note <> '') and det.gecko_id = (select REPLACE(mst.gecko_id, 'DAY', 'HOUR')   from orders mst where det.trend=mst.trend and mst.gecko_id=REPLACE(det.gecko_id, 'HOUR'  , 'DAY')) ORDER BY det.trend, det.gecko_id ", nativeQuery = true)
     public List<Orders> getTrend_H1List();
 }
