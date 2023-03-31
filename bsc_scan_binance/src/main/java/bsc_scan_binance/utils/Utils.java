e7db35a7b8a2291240e2463c1a3844ea8ae071e1
khanhduyapt <khanhduyapt@gmail.com>
2023-03-31 09:18:57 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/utils/Utils.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/utils/Utils.java
@@ -62,7 +62,7 @@ import bsc_scan_binance.response.MoneyAtRiskResponse;
 public class Utils {
 
     public static final BigDecimal ACCOUNT = BigDecimal.valueOf(20000);
-    public static final BigDecimal RISK_PERCENT = BigDecimal.valueOf(0.01);
+    public static final BigDecimal RISK_PERCENT = BigDecimal.valueOf(0.005);
 
     public static final String chatId_duydk = "5099224587";
     public static final String chatUser_duydk = "tg25251325";
