b250c20d9401010f41da0a9069a55ffc530e71a4
khanhduyapt <khanhduyapt@gmail.com>
2023-04-12 00:28:59 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -3386,7 +3386,7 @@ public class BinanceServiceImpl implements BinanceService {
                             result_03 += ", ";
                         }
                         result_03 += Utils.appendSpace(type + EPIC, 15);
-                        outputLog(EPIC, dto_h1, dto_h4, "(Swing) " + dto_05.getNote());
+                        outputLog(EPIC, dto_05, dto_h4, "(Swing) " + dto_05.getNote());
                         has_output = true;
                     }
                 }
@@ -3399,14 +3399,14 @@ public class BinanceServiceImpl implements BinanceService {
                     }
                     result_03 += Utils.appendSpace(type + EPIC, 15);
 
-                    outputLog(EPIC, dto_h1, dto_h4, "(Scap ) " + dto_05.getNote());
+                    outputLog(EPIC, dto_05, dto_h4, "(Scap ) " + dto_05.getNote());
                     has_output = true;
                 }
 
                 if (!has_output && Objects.equals(trend_h1, trend_15) && Objects.equals(trend_15, trend_05)
                         && Utils.isNotBlank(dto_05.getNote())) {
 
-                    outputLog(EPIC, dto_h1, dto_h4, "(     ) " + dto_05.getNote());
+                    outputLog(EPIC, dto_05, dto_h4, "(     ) " + dto_05.getNote());
                 }
 
             }
