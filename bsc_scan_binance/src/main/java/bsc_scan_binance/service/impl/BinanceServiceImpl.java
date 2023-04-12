48647a2092fd55ad62446ee43bb46188f7ee9610
khanhduyapt <khanhduyapt@gmail.com>
2023-04-12 19:59:02 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -3295,19 +3295,28 @@ public class BinanceServiceImpl implements BinanceService {
         }
 
         if (Objects.equals(Utils.CAPITAL_TIME_MINUTE_5, CAPITAL_TIME_XX)) {
-            boolean isAboveMa50 = Utils.isAboveMALine(heken_list, 50);
-            if (Objects.equals(Utils.TREND_LONG, trend) && !isAboveMa50) {
-                if (Utils.isBlank(note)) {
-                    note = Utils.getChartNameCapital(CAPITAL_TIME_XX) + Utils.appendSpace(trend, 4);
-                }
-                note += Utils.TEXT_SWITCH_TREND_BELOW_50_LONG;
+            String switch_trend = Utils.switchTrendByMaXX(heken_list, 1, 50);
+            switch_trend += Utils.switchTrendByMaXX(heken_list, 2, 50);
+            switch_trend += Utils.switchTrendByMaXX(heken_list, 3, 50);
+
+            if (Utils.isNotBlank(switch_trend)) {
+                note = Utils.getChartNameCapital(CAPITAL_TIME_XX) + Utils.appendSpace(trend, 4)
+                        + Utils.TEXT_SWITCH_TREND_BY_Ma50;
             }
-            if (Objects.equals(Utils.TREND_SHORT, trend) && isAboveMa50) {
-                if (Utils.isBlank(note)) {
-                    note = Utils.getChartNameCapital(CAPITAL_TIME_XX) + Utils.appendSpace(trend, 4);
-                }
-                note += Utils.TEXT_SWITCH_TREND_ABOVE_50_SHOT;
-            }
+
+            //boolean isAboveMa50 = Utils.isAboveMALine(heken_list, 50);
+            //if (Objects.equals(Utils.TREND_LONG, trend) && !isAboveMa50) {
+            //    if (Utils.isBlank(note)) {
+            //        note = Utils.getChartNameCapital(CAPITAL_TIME_XX) + Utils.appendSpace(trend, 4);
+            //    }
+            //    note += Utils.TEXT_SWITCH_TREND_BELOW_50_LONG;
+            //}
+            //if (Objects.equals(Utils.TREND_SHORT, trend) && isAboveMa50) {
+            //    if (Utils.isBlank(note)) {
+            //        note = Utils.getChartNameCapital(CAPITAL_TIME_XX) + Utils.appendSpace(trend, 4);
+            //    }
+            //    note += Utils.TEXT_SWITCH_TREND_ABOVE_50_SHOT;
+            //}
         }
 
         // -----------------------------DATABASE---------------------------
@@ -3323,13 +3332,14 @@ public class BinanceServiceImpl implements BinanceService {
 
         BigDecimal str_body_price = low_high.get(0);
         BigDecimal end_body_price = low_high.get(1);
-        if (CAPITAL_TIME_XX.contains("MINUTE_") || Objects.equals(Utils.CAPITAL_TIME_HOUR, CAPITAL_TIME_XX)) {
-            //List<BigDecimal> body = Utils.getOpenCloseCandle(list.subList(0, 10));
-            //str_body_price = body.get(0);
-            //end_body_price = body.get(1);
-
+        if (CAPITAL_TIME_XX.contains("MINUTE_")) {
             str_body_price = list.get(0).getCurrPrice();
             end_body_price = list.get(0).getCurrPrice();
+        }
+        if (Objects.equals(Utils.CAPITAL_TIME_HOUR, CAPITAL_TIME_XX)) {
+            List<BigDecimal> body = Utils.getOpenCloseCandle(list.subList(0, 10));
+            str_body_price = body.get(0);
+            end_body_price = body.get(1);
         }
 
         BigDecimal sl_long = low_high.get(0).subtract(bread);
@@ -3373,40 +3383,34 @@ public class BinanceServiceImpl implements BinanceService {
                 String type = Objects.equals(Utils.TREND_LONG, trend_05) ? "(B)"
                         : Objects.equals(Utils.TREND_SHORT, trend_05) ? "(S)" : "(x)";
 
-                // TODO: scapForex
+                // TODO: Bat buoc phai danh theo khung D1, khong co keo thi nghi.
+                // (Da chay 3 tai khoan 20k vi danh nguoc xu huong D1 vi tham bat dinh bat day)
                 boolean has_output = false;
                 if (Utils.isNotBlank(dto_h4.getNote())
+                        && Objects.equals(TREND_D1, trend_h4)
                         && Objects.equals(trend_h4, trend_h1)
-                        && Objects.equals(trend_h1, trend_15) && Objects.equals(trend_15, trend_05)) {
-
-                    if (Utils.isNotBlank(dto_15.getNote())) {
-                        if (Utils.isNotBlank(result_03)) {
-                            result_15 += ", ";
-                        }
-                        result_15 += Utils.appendSpace(type + EPIC, 15);
-                        outputLog(EPIC, dto_15, dto_h4, "(Swing) " + dto_15.getNote());
-                        has_output = true;
-                    }
+                        && Objects.equals(trend_h1, trend_15)
+                        && Objects.equals(trend_15, trend_05)) {
 
                     if (Utils.isNotBlank(dto_05.getNote())) {
                         if (Utils.isNotBlank(result_03)) {
                             result_03 += ", ";
                         }
                         result_03 += Utils.appendSpace(type + EPIC, 15);
-                        outputLog(EPIC, dto_05, dto_h4, "(Swing) " + dto_05.getNote());
+                        outputLog(EPIC, dto_05, dto_h4, dto_h4.getNote());
                         has_output = true;
                     }
                 }
 
-                if (!has_output && Objects.equals(TREND_D1, trend_h4) && Objects.equals(TREND_D1, trend_15)) {
-                    if (Utils.isNotBlank(dto_15.getNote())) {
-                        outputLog(EPIC, dto_15, dto_h4, "(     ) " + dto_15.getNote());
-                    }
+                if (!has_output && Objects.equals(TREND_D1, trend_h4) && Objects.equals(trend_h4, trend_05)
+                        && dto_05.getNote().contains(Utils.TEXT_SWITCH_TREND_BY_Ma50)) {
 
-                    if (Objects.equals(TREND_D1, trend_05)
-                            && Utils.isNotBlank(dto_05.getNote())) {
-                        outputLog(EPIC, dto_05, dto_h4, "(     ) " + dto_05.getNote());
+                    if (Utils.isNotBlank(result_03)) {
+                        result_03 += ", ";
                     }
+                    result_03 += Utils.appendSpace(type + EPIC, 15);
+
+                    outputLog(EPIC, dto_05, dto_h4, dto_05.getNote());
                 }
 
             }
