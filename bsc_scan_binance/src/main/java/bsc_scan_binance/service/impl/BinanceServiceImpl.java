ba15c279f3633de6268b1a301322c70fdccd87d1
khanhduyapt <khanhduyapt@gmail.com>
2023-06-22 22:38:57 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -4119,6 +4119,14 @@ public class BinanceServiceImpl implements BinanceService {
                     dto = Utils.calc_Lot_En_SL_TP(EPIC, action, dto_05, dto_h4, Utils.CAPITAL_TIME_H4, append, true);
                 }
 
+                if ((Objects.isNull(dto) || !Objects.equals(action, trend_h12))
+                        && (m05_allow_trade || m15_allow_trade || Utils.isNotBlank(note_05))
+                        && Objects.equals(trend_d1, trend_h12) && Objects.equals(trend_h12, trend_05)) {
+                    action = trend_h12;
+                    append += ".241205";
+                    dto = Utils.calc_Lot_En_SL_TP(EPIC, action, dto_05, dto_h4, Utils.CAPITAL_TIME_H4, append, true);
+                }
+
                 // ---------------------------------------------------------------------
                 if ((Objects.isNull(dto) || !Objects.equals(action, trend_h12)) && m05_allow_trade
                         && Objects.equals(trend_h12, trend_h1) && Objects.equals(trend_h1, trend_15)
