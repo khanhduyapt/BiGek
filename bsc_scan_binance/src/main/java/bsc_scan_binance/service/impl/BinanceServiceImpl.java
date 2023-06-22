4e21964acdb38b5e1874dd565e867cf79c05965e
khanhduyapt <khanhduyapt@gmail.com>
2023-06-22 16:19:16 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -2974,7 +2974,7 @@ public class BinanceServiceImpl implements BinanceService {
 
         // ----------------------------------------PROFIT--------------------------------------
         BigDecimal risk = Utils.ACCOUNT.multiply(Utils.RISK_PERCENT);
-        // BigDecimal profit_2R = risk.multiply(BigDecimal.valueOf(2));
+        BigDecimal profit_05R = risk.divide(BigDecimal.valueOf(2), 1, RoundingMode.CEILING);
         BigDecimal profit_1R = risk;
 
         for (Mt5DataTrade trade : tradeList) {
@@ -3000,11 +3000,14 @@ public class BinanceServiceImpl implements BinanceService {
             }
 
             // Dừng trade khi H1_1 đóng nến tại LoHi + Bread1-5 của H1.
+            Orders dto_d1 = ordersRepository.findById(EPIC + "_" + Utils.CAPITAL_TIME_D1).orElse(null);
+            Orders dto_h12 = ordersRepository.findById(EPIC + "_" + Utils.CAPITAL_TIME_H12).orElse(dto_d1);
             Orders dto_h4 = ordersRepository.findById(EPIC + "_" + Utils.CAPITAL_TIME_H4).orElse(null);
             Orders dto_h1 = ordersRepository.findById(EPIC + "_" + Utils.CAPITAL_TIME_H1).orElse(null);
             Orders dto_15 = ordersRepository.findById(EPIC + "_" + Utils.CAPITAL_TIME_15).orElse(dto_h1);
             Orders dto_05 = ordersRepository.findById(EPIC + "_" + Utils.CAPITAL_TIME_05).orElse(dto_h1);
-            if (Objects.isNull(dto_h4) || Objects.isNull(dto_h1) || Objects.isNull(dto_15) || Objects.isNull(dto_05)) {
+            if (Objects.isNull(dto_d1) || Objects.isNull(dto_h4) || Objects.isNull(dto_h1) || Objects.isNull(dto_15)
+                    || Objects.isNull(dto_05)) {
                 Utils.logWritelnDraft("monitorProfit Orders dto is NULL " + EPIC);
                 continue;
             }
@@ -3032,6 +3035,8 @@ public class BinanceServiceImpl implements BinanceService {
             BigDecimal TP_order = mt5Entity.getTakeProfit();
             BigDecimal SL_tf = mt5Entity.getStopLossTimeFam();
 
+            String trend_d1 = dto_d1.getTrend();
+            String trend_h12 = dto_h12.getTrend();
             String trend_h1 = dto_h1.getTrend();
             String trend_15 = dto_15.getTrend();
             String trend_05 = dto_05.getTrend();
@@ -3099,6 +3104,13 @@ public class BinanceServiceImpl implements BinanceService {
                 }
             }
 
+            if (PROFIT.add(profit_05R).compareTo(BigDecimal.ZERO) < 0) {
+                if ((Objects.equals(trend_d1, trend_h12) && Objects.equals(trend_h12, trend_h4)
+                        && Objects.equals(trend_h4, trend_h1) && Objects.equals(trend_h1, trend_15)
+                        && Objects.equals(trend_15, trend_05) && !Objects.equals(trend_d1, TRADE_TREND))) {
+                    isInverseTrend = true;
+                }
+            }
             // ---------------------------------------------------------------------------------
             if (isPriceHit_SL || isPriceHit_TP || isInverseTrend) {
                 String prefix = Utils.getChartNameCapital(mt5Entity.getTimeframe()) + "Closed.   ";
