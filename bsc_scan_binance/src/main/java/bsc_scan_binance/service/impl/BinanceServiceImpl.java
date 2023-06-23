37fffac7d279b406e1ff79d1711edb1e60af346a
khanhduyapt <khanhduyapt@gmail.com>
2023-06-23 12:20:13 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -4029,20 +4029,21 @@ public class BinanceServiceImpl implements BinanceService {
                 if (Objects.equals(trend_15, trend_05) && (m05_allow_trade || m15_allow_trade
                         || Utils.isNotBlank(note_05) || Utils.isNotBlank(note_15))) {
 
-                    if (Objects.equals(bread_trend_h12, bread_trend_h4)
-                            && Objects.equals(bread_trend_h4, bread_trend_h1)
-                            && Objects.equals(bread_trend_h1, bread_trend_15)
-                            && Objects.equals(bread_trend_15, bread_trend_05)
-                            && Objects.equals(bread_trend_05, trend_05)) {
-                        action = trend_15;
-                        append = ".000015";
-                        dto = Utils.calc_Lot_En_SL_TP(EPIC, action, dto_05, dto_d1, Utils.CAPITAL_TIME_H1, append,
-                                true);
+                    if (Utils.isNotBlank(note_d1 + note_h12 + note_h4)) {
+                        if (Objects.equals(bread_trend_h12, bread_trend_h4)
+                                && Objects.equals(bread_trend_h4, bread_trend_h1)
+                                && Objects.equals(bread_trend_h1, bread_trend_15)
+                                && Objects.equals(bread_trend_15, bread_trend_05)
+                                && Objects.equals(bread_trend_05, trend_05)) {
+                            action = trend_15;
+                            append = ".000015";
+                            dto = Utils.calc_Lot_En_SL_TP(EPIC, action, dto_05, dto_d1, Utils.CAPITAL_TIME_H1, append,
+                                    true);
+                        }
                     }
 
                     if (Objects.isNull(dto) || !Objects.equals(action, trend_h4)) {
-                        if (Objects.equals(bread_trend_h12, trend_h4) && Objects.equals(trend_h4, trend_h1)
-                                && Objects.equals(trend_h1, trend_15)) {
+                        if (Objects.equals(trend_h4, trend_h1) && Objects.equals(trend_h1, trend_15)) {
                             action = trend_h4;
                             append = ".004100";
                             dto = Utils.calc_Lot_En_SL_TP(EPIC, action, dto_05, dto_d1, Utils.CAPITAL_TIME_H4, append,
