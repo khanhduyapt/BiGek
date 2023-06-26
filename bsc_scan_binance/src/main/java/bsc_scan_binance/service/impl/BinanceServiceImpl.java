9f82641f00d5d20e53af104ffa3dd4e6136539be
khanhduyapt <khanhduyapt@gmail.com>
2023-06-26 20:05:04 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -4185,18 +4185,13 @@ public class BinanceServiceImpl implements BinanceService {
                 if (!Objects.equals(trend_h1, TRADE_TREND) && !Objects.equals(trend_15, TRADE_TREND)
                         && !Objects.equals(trend_05, TRADE_TREND)) {
 
-                    if (Objects.equals(mt5Entity.getTimeframe(), Utils.CAPITAL_TIME_H1)) {
-                        isTrendInverse = true;
-
-                    } else if (Objects.equals(mt5Entity.getTimeframe(), Utils.CAPITAL_TIME_D1)) {
+                    if (Objects.equals(mt5Entity.getTimeframe(), Utils.CAPITAL_TIME_D1)) {
                         if (!Objects.equals(dto_h12.getTrend(), TRADE_TREND)
                                 && !Objects.equals(trend_h4, TRADE_TREND)) {
                             isTrendInverse = true;
                         }
-                    } else {
-                        if (!Objects.equals(trend_h4, TRADE_TREND)) {
-                            isTrendInverse = true;
-                        }
+                    } else if (!Objects.equals(trend_h4, TRADE_TREND)) {
+                        isTrendInverse = true;
                     }
                 }
 
