c076a03d268468f08f3cbbf6cdb3d5384a8f522f
khanhduyapt <khanhduyapt@gmail.com>
2023-07-16 00:27:14 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -3637,16 +3637,19 @@ public class BinanceServiceImpl implements BinanceService {
             type = Utils.switchTrendByHeken_12(heiken_list);
         }
 
-        if (Utils.isBlank(type)) {
-            String switch_trend = Utils.switchTrendByMaXX(heiken_list, 3, 5);
-            if (Utils.isNotBlank(switch_trend) && switch_trend.contains(trend)) {
-                type = Utils.appendSpace(trend, 4) + Utils.TEXT_SWITCH_TREND_Ma_3_5;
+        if (Objects.equals(CAPITAL_TIME_XX, Utils.CAPITAL_TIME_H1)
+                || Objects.equals(CAPITAL_TIME_XX, Utils.CAPITAL_TIME_H4)) {
+            if (Utils.isBlank(type)) {
+                String switch_trend = Utils.switchTrendByMaXX(heiken_list, 3, 5);
+                if (Utils.isNotBlank(switch_trend) && switch_trend.contains(trend)) {
+                    type = Utils.appendSpace(trend, 4) + Utils.TEXT_SWITCH_TREND_Ma_3_5;
+                }
             }
-        }
-        if (Utils.isBlank(type)) {
-            String switch_trend = Utils.switchTrendByMaXX(heiken_list, 1, 10);
-            if (Utils.isNotBlank(switch_trend) && switch_trend.contains(trend)) {
-                type = Utils.appendSpace(trend, 4) + Utils.TEXT_SWITCH_TREND_Ma_1_10;
+            if (Utils.isBlank(type)) {
+                String switch_trend = Utils.switchTrendByMaXX(heiken_list, 1, 10);
+                if (Utils.isNotBlank(switch_trend) && switch_trend.contains(trend)) {
+                    type = Utils.appendSpace(trend, 4) + Utils.TEXT_SWITCH_TREND_Ma_1_10;
+                }
             }
         }
 
