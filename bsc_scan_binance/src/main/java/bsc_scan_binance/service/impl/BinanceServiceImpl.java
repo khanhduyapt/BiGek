83f8dfa6f7e8269f7574f76f33dd0ad1990be484
khanhduyapt <khanhduyapt@gmail.com>
2023-08-01 10:37:03 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -4097,20 +4097,6 @@ public class BinanceServiceImpl implements BinanceService {
 
                 Mt5OpenTrade trade_h4 = null;
 
-                // Từ triệu phú thành tay trắng do đánh W & D nghịch pha nhau.
-                if (is_candidate && m5_allow_trade && (m15_allow_trade || is_sweet_trend)) {
-                    String key = EPIC + Utils.CAPITAL_TIME_H4;
-                    String append = "962412_040105" + text_risk_010;
-
-                    trade_h4 = Utils.calc_Lot_En_SL_TP(Utils.RISK_0_10_PERCENT, EPIC, trend_d1, dto_15, dto_h4, append,
-                            true, Utils.CAPITAL_TIME_H4);
-
-                    trade_h4.setComment(trade_h4.getComment() + Utils.TEXT_EQ_WDH12);
-
-                    BscScanBinanceApplication.mt5_open_trade_List.add(trade_h4);
-                    BscScanBinanceApplication.dic_comment.put(key, trade_h4.getComment());
-                }
-
                 if (Objects.isNull(trade_h4) && is_trade_zone && m15_allow_trade && dto_h4.isAllow_trade_by_ma50()
                         && dto_h4.getSwitch_trend().contains(Utils.TEXT_SWITCH_TREND_Ma_1vs6810)) {
                     isMa_1vs6810 = true;
@@ -4136,6 +4122,21 @@ public class BinanceServiceImpl implements BinanceService {
                     BscScanBinanceApplication.mt5_open_trade_List.add(trade_h4);
                     BscScanBinanceApplication.dic_comment.put(key, trade_h4.getComment());
                 }
+
+                // Từ triệu phú thành tay trắng do đánh W & D nghịch pha nhau.
+                if (Objects.isNull(trade_h4) && is_candidate && m5_allow_trade && m15_allow_trade && is_sweet_trend) {
+                    String key = EPIC + Utils.CAPITAL_TIME_H4;
+                    String append = "962412_040105" + text_risk_010;
+
+                    trade_h4 = Utils.calc_Lot_En_SL_TP(Utils.RISK_0_10_PERCENT, EPIC, trend_d1, dto_15, dto_h4, append,
+                            true, Utils.CAPITAL_TIME_H4);
+
+                    trade_h4.setComment(trade_h4.getComment() + Utils.TEXT_EQ_WDH12);
+
+                    BscScanBinanceApplication.mt5_open_trade_List.add(trade_h4);
+                    BscScanBinanceApplication.dic_comment.put(key, trade_h4.getComment());
+                }
+
             }
 
             // ---------------------------------------------------------------------
