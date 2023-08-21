aeb06a87a4a1f87f245aa24353f21ff02a2e244d
khanhduyapt <khanhduyapt@gmail.com>
2023-08-21 23:01:01 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/service/impl/BinanceServiceImpl.java
@@ -3951,7 +3951,7 @@ public class BinanceServiceImpl implements BinanceService {
             }
             boolean is_hit_sl = false;
             boolean no_stop_loss = (trade.getStopLoss().compareTo(BigDecimal.ZERO) == 0);
-            if (no_stop_loss && PROFIT.add(Utils.RISK_PER_TRADE.multiply(BigDecimal.valueOf(1.5)))
+            if (no_stop_loss && PROFIT.add(Utils.RISK_PER_TRADE.multiply(BigDecimal.valueOf(2)))
                     .compareTo(BigDecimal.ZERO) < 0) {
                 is_hit_sl = true;
             }
