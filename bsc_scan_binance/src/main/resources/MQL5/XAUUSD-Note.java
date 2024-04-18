2710c091798d0bd2e648502f543c5a49bdd63e1e
khanhduyapt <khanhduyapt@gmail.com>
2024-04-18 09:59:07 +0700

    add Cent16

--- bsc_scan_binance/src/main/resources/MQL5/XAUUSD-Note.java
+++ bsc_scan_binance/src/main/resources/MQL5/XAUUSD-Note.java
@@ -23,6 +23,8 @@ C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\3D65A11D0F648332DBC0B8072
 C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\1A80FA4822AD116C13E4D2F7729CC36D\MQL4\Experts
 C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\1A80FA4822AD116C13E4D2F7729CC36D\MQL4\Experts\XAUUSD-Cent15.mq4
 
+C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\279DE0AE13CF614039E1C3C0F1DBC5FA\MQL4\Experts
+C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\279DE0AE13CF614039E1C3C0F1DBC5FA\MQL4\Experts\XAUUSD-Cent16.mq4
 
 C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\5C26F8515847D453D9060878036A0127\MQL4\Experts
 C:\Users\DellE5270\AppData\Roaming\MetaQuotes\Terminal\5C26F8515847D453D9060878036A0127\MQL4\Experts\XAUUSD-Cent17.mq4
